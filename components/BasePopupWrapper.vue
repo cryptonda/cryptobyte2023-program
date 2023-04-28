444279f64335b6bd36f6d74614de8cd15ab96056
Klein Petr <petr@kleinpetr.com>
2023-04-28 10:56:56 +0200

    add modal for desktop

--- components/BasePopupWrapper.vue
+++ components/BasePopupWrapper.vue
@@ -1,12 +1,27 @@
 <script lang="ts" setup>
+import { useMediaQuery } from '@vueuse/core'
+import type { ModalSize } from './BaseModal.vue'
+
+import BaseModal from './BaseModal.vue'
+import BaseBottomSheet from './BaseBottomSheet.vue'
+
+// unocss safelist
+// md:w-720px md:w-920px md:w-400px
+// NFT Miss Figma requires md:w-800px :)
+
 interface Props {
   modelValue?: boolean
   isClosable?: boolean
+  size?: ModalSize
+  bgColor?: string
+  initialFocus?: HTMLElement
 }
 
 const props = withDefaults(defineProps<Props>(), {
   modelValue: false,
   isClosable: true,
+  size: '800px',
+  bgColor: '',
 })
 
 const emit = defineEmits<{
@@ -23,12 +38,14 @@ function close(): void {
   isOpen.value = false
   emit('close')
 }
+
+const isDesktop = useMediaQuery('(min-width: 640px)')
 </script>
 
 <template>
   <ClientOnly>
-    <BaseBottomSheet v-model="isOpen" :is-closable="isClosable" @close="close">
+    <Component :is="isDesktop ? BaseModal : BaseBottomSheet" v-model="isOpen" :is-closable="isClosable" :size="size" :bg-color="bgColor" :initial-focus="initialFocus" @close="close">
       <slot />
-    </BaseBottomSheet>
+    </Component>
   </ClientOnly>
 </template>
